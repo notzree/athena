30e474381ad86d83c6f6a9798ab2e376f66af6c5
Annie Cai <caishajia@gmail.com>
2023-05-16 23:11:06 -0400

    added a dot...

--- athena-webapp/app/(auth-group)/layout.tsx
+++ athena-webapp/app/(auth-group)/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
+import '../global.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
--- athena-webapp/app/(unauth-group)/layout.tsx
+++ athena-webapp/app/(unauth-group)/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
