19fb50a14881ae83059f0217c57f9a85f7b99335
Annie Cai <caishajia@gmail.com>
2023-05-17 14:14:47 -0400

    completed navbar for mobile+desktop

--- athena-webapp/app/(unauth-group)/layout.tsx
+++ athena-webapp/app/(unauth-group)/layout.tsx
@@ -1,6 +1,9 @@
+
 import '../globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import NavBar from '@/components/NavBar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,11 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-
-      <body className={inter.className}>{children}</body>
       
+    <html lang="en">
+      <body className={inter.className}>
+        <nav>
+          <NavBar/>
+        </nav>
+        {children}
+        </body>
     </html>
     </ClerkProvider>
   )
 }
+
