26552c283ce8b4284760324a9671f617050e8cae
Annie Cai <caishajia@gmail.com>
2023-05-28 15:00:01 -0400

    mobile burger sidebar

--- athena-webapp/components/DashboardSidebar.tsx
+++ athena-webapp/components/DashboardSidebar.tsx
@@ -6,7 +6,6 @@ import {
   ListItem,
   ListItemPrefix,
   IconButton,
-  Navbar,
 } from "@material-tailwind/react";
 import {
   Squares2X2Icon,
@@ -16,6 +15,8 @@ import {
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Logo from "../public/light-logo.png";
+import LogoDark from "../public/dark.png";
+
 import Link from "next/link";
 import LogoutButton from "./LogoutButton";
 import { useEffect, useState } from "react";
@@ -26,30 +27,30 @@ function SidebarList() {
     <List className="p-10">
       <ListItem className="text-xl py-3">
         <ListItemPrefix>
-          <Squares2X2Icon className="h-5 w-5 mr-2" />
+          <Squares2X2Icon className="h-5 w-5 mx-3" />
         </ListItemPrefix>
         Dashboard
       </ListItem>
       <ListItem className="text-xl py-3">
         <ListItemPrefix>
-          <PencilSquareIcon className="h-5 w-5 mr-2" />
+          <PencilSquareIcon className="h-5 w-5 mx-3" />
         </ListItemPrefix>
         Questionnaire
       </ListItem>
       <ListItem className="text-xl py-3">
         <ListItemPrefix>
-          <UserCircleIcon className="h-5 w-5 mr-2" />
+          <UserCircleIcon className="h-5 w-5 mx-3" />
         </ListItemPrefix>
         Profile
       </ListItem>
       <ListItem className="text-xl py-3">
         <ListItemPrefix>
-          <InboxIcon className="h-5 w-5 mr-2" />
+          <InboxIcon className="h-5 w-5 mx-3" />
         </ListItemPrefix>
         Results
       </ListItem>
       <ListItem>
-        <div className="md:absolute md:bottom-16 py-3">
+        <div className="absolute md:bottom-16 bottom-[20vh]">
           <LogoutButton />
         </div>
       </ListItem>
@@ -58,17 +59,35 @@ function SidebarList() {
 }
 export default function DashboardSidebar() {
   const [openSidebar, setOpenSidebar] = useState(false);
+  const [deviceSize, setDeviceSize] = useState(false);
 
   const handleWindowResize = () =>
     window.innerWidth >= 960 && setOpenSidebar(false);
 
+  const checkSideBar = () => {
+    const screenWidth =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    console.log(screenWidth);
+    if (screenWidth <= 786) {
+      setDeviceSize(true);
+    } else {
+      setDeviceSize(false);
+    }
+  };
+
   useEffect(() => {
     window.addEventListener("resize", handleWindowResize);
+    window.addEventListener("resize", checkSideBar);
 
     return () => {
       window.removeEventListener("resize", handleWindowResize);
+      window.removeEventListener("resize", checkSideBar);
     };
   }, []);
+
+  console.log("Size" + deviceSize);
 
   return (
     <>
@@ -92,30 +111,38 @@ export default function DashboardSidebar() {
         </div>
         <SidebarList />
       </Card>
-      {/* {
-        <div className="w-screen px-6 py-3">
-          <IconButton
-            variant="text"
-            className="float-right m-5 z-60 text-neutral-800 hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpenSidebar(!openSidebar)}
-          >
-            {openSidebar ? (
-              <XMarkIcon className="h-6 w-6" strokeWidth={2} />
-            ) : (
-              <Bars3Icon className="h-6 w-6" strokeWidth={2} />
-            )}
-          </IconButton>
-
+      {deviceSize && (
+        <div className="top-0 absolute w-screen bg-violet-50 px-6 py-3">
           <div
             className={`top-24 left-0 w-full bg-violet-50 px-5 text-neutral-800 fixed h-full overflow-hidden z-10 ease-in-out duration-300 ${
               openSidebar ? "translate-x-full " : "translate-x-0"
             }`}
           >
+            <Link href="/">
+              <Image
+                width="70"
+                height="70"
+                src={LogoDark}
+                alt="athena dark logo"
+              />
+            </Link>
+            <IconButton
+              variant="text"
+              className="ml-auto m-5 z-60 text-neutral-800 hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenSidebar(!openSidebar)}
+            >
+              {openSidebar ? (
+                <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+              ) : (
+                <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+              )}
+            </IconButton>
+
             <SidebarList />
           </div>
         </div>
-      } */}
+      )}
     </>
   );
 }
