fe4281ee059da8f784a2b28ec402a6c8779fe3f9
Annie Cai <caishajia@gmail.com>
2023-05-28 15:48:57 -0400

    Revert "Annie/ath 34 create logged in page" (#12)

--- athena-webapp/app/(auth-group)/dashboard/layout.tsx
+++ athena-webapp/app/(auth-group)/dashboard/layout.tsx
@@ -1,26 +1,14 @@
-import DashboardSidebar from "@/components/DashboardSidebar";
-
 export default function DashboardLayout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-        rel="stylesheet"
-      />
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
       <section>
         {/* Include shared UI here e.g. a header or sidebar */}
-        <nav>
-          <DashboardSidebar />
-        </nav>
-
+        <nav></nav>
+   
         {children}
       </section>
-    </>
-  );
-}
+    );
+  }
